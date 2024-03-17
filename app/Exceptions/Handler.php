408062d20fde566c6447c3ef7e7d9e7de0d5a1e2
Andres <andresf150@gmail.com>
2024-03-17 12:40:02 -0600

    respuesta al no enctrar datos

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -26,5 +27,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exeption)
+    {
+        if($exeption instanceof ModelNotFoundException){
+            return response()->json(
+                ["res" => false,
+                "error" => "Error datos no encontrados"],400);
+        }
+        return parent::render($request,$exeption);
     }
 }
